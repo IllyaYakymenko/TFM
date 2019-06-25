6901e8a59fcbcc382f6c017e8d3c2217b59823f4
IllyaYakymenko <46624284+IllyaYakymenko@users.noreply.github.com>
2019-06-25 11:33:34 +0200

    Add files via upload

---
+++ correlaciones.f90
@@ -0,0 +1,190 @@
+!	############################################################################
+!	#
+!	#   Obtención del diagrama de Ramachandran para cada
+!	#   aminoácido.
+!	#   Input: Channel 10 (fichero _raman.dat)
+!	#	Output: Channel 20: Mapa de correlaciones de todos los aminoácidos
+!	#					21-> 20 + número de aminoácidos:
+!	#						Mapa de correlaciones de cada aminoácido
+!	#                       Canales dependientes de los aminoácidos de análisis
+!	#
+!	############################################################################
+
+
+program mapa_correlaciones
+
+implicit none
+
+! Datos de entrada (archivo.in)
+
+integer :: numfiles, resol, numrow, numcol, PhiRelAA, PsiRelAA
+character*200, allocatable :: filenames(:)
+real :: PhiMin, PhiMax, PsiMin, PsiMax
+
+! Definición de la matriz del diagrama
+
+double precision, allocatable :: correlaciones(:,:,:), SPhiPsi(:,:,:), SPhi(:,:,:) 
+double precision, allocatable :: SPsi(:,:,:), SPhiCuad(:,:,:), SPsiCuad(:,:,:) 
+integer, allocatable :: N(:,:,:)
+double precision :: rangoPhi, rangoPsi, despPhi, despPsi, dSPhi, dSPsi, dSPhiPsi
+integer :: diagPhi, diagPsi, numaa
+double precision, allocatable :: diedros(:), diedrosant(:), desplazamientos(:)
+integer, allocatable :: outputs(:)
+
+! Variables de control
+
+integer ch, row, col, fileind, rowiter, dr, dc, aa, i, diagaa, waa, from, to, regaa, di
+
+read(5,*) numfiles
+
+allocate(filenames(numfiles))
+
+read(5,*) filenames
+read(5,*) numrow
+read(5,*) numcol
+read(5,*) PhiMin, PhiMax, PsiMin, PsiMax
+read(5,*) PhiRelAA, PsiRelAA
+read(5,*) resol
+
+numaa = numcol/2
+
+from = 1
+to = numaa
+
+if ((PhiRelAA.lt.0).or.(PsiRelAA.lt.0)) then
+	from = 1 - min(PhiRelAA, PsiRelAA)
+end if
+
+if ((PhiRelAA.gt.0).or.(PsiRelAA.gt.0)) then
+	to = numaa - max(PhiRelAA, PsiRelAA)
+end if
+
+regaa = to - from +1
+
+allocate(correlaciones(from:to+1,resol,resol))
+allocate(SPhiPsi(from:to+1,resol,resol))
+allocate(SPhi(from:to+1,resol,resol))
+allocate(SPsi(from:to+1,resol,resol))
+allocate(SPhiCuad(from:to+1,resol,resol))
+allocate(SPsiCuad(from:to+1,resol,resol))
+allocate(N(from:to+1,resol,resol))
+allocate(diedros(numcol))
+allocate(diedrosant(numcol))
+allocate(desplazamientos(numcol))
+allocate(outputs(from:to))
+
+! Creación de canales de output
+! En outputs se guardan por orden las salidas de cada amonoácido
+! Hay tantos outputs como aminoácidos en el polipéptido
+! Las frecuencias de todos los aminoácidos se guardan en la posición
+! numaa+1 de la primera dimensión de diagrama
+
+do ch=from, to
+	outputs(ch) = 20+ch	
+end do
+
+! Matrices vacias 
+do diagaa=from, to+1
+	do row=1, resol
+		do col=1, resol
+			correlaciones(diagaa,row, col) = 0.d0
+			SPhiPsi(diagaa,row, col) = 0.d0
+			SPhi(diagaa,row, col) = 0.d0
+			SPsi(diagaa,row, col) = 0.d0
+			SPhiCuad(diagaa,row, col) = 0.d0
+			SPsiCuad(diagaa,row, col) = 0.d0
+			N(diagaa,row, col) = 0.d0
+		end do
+	end do
+end do
+
+rangoPhi = (PhiMax - PhiMin)/resol
+rangoPsi = (PsiMax - PsiMin)/resol
+
+print*, 'Calculando mapas de correlaciones'
+
+do fileind=1, numfiles
+	open(10, file = filenames(fileind))
+	read(10,*) diedrosant
+
+	do rowiter=2, numrow
+		read(10,*) diedros
+
+		do di=1, numcol
+		desplazamientos(di) = (diedros(di) - diedrosant(di))-1*int((diedros(di) - diedrosant(di))/180)*360	
+		end do
+
+		do aa=from, to
+			! Posición en las matrices
+			diagPhi = int((diedros(2*aa-1)+PhiMax)/rangoPhi)+1
+			diagPsi = int((diedros(2*aa)+PsiMax)/rangoPsi)+1
+			
+			if(diagPhi.ge.resol) then
+				diagPhi=resol
+			end if
+
+			if(diagPsi.ge.resol) then
+				diagPsi=resol
+			end if
+
+			! Desplazamiento del ángulo solicitado
+			despPhi = desplazamientos(2*aa+2*PhiRelAA-1)
+			despPsi = desplazamientos(2*aa+2*PsiRelAA)
+
+			! Registro de los desplazamientos en las distintas matrices
+			SPhiPsi(aa, diagPsi, diagPhi) = SPhiPsi(aa, diagPsi, diagPhi) + despPhi*despPsi
+			SPhi(aa, diagPsi, diagPhi) = SPhi(aa, diagPsi, diagPhi) + despPhi
+			SPsi(aa, diagPsi, diagPhi) = SPsi(aa, diagPsi, diagPhi) + despPsi
+			SPhiCuad(aa, diagPsi, diagPhi) = SPhiCuad(aa, diagPsi, diagPhi) + despPhi*despPhi
+			SPsiCuad(aa, diagPsi, diagPhi) = SPsiCuad(aa, diagPsi, diagPhi) + despPsi*despPsi
+			N(aa, diagPsi, diagPhi) = N(aa, diagPsi, diagPhi) + 1
+
+			SPhiPsi(to+1, diagPsi, diagPhi) = SPhiPsi(to+1, diagPsi, diagPhi) + despPhi*despPsi
+			SPhi(to+1, diagPsi, diagPhi) = SPhi(to+1, diagPsi, diagPhi) + despPhi
+			SPsi(to+1, diagPsi, diagPhi) = SPsi(to+1, diagPsi, diagPhi) + despPsi
+			SPhiCuad(to+1, diagPsi, diagPhi) = SPhiCuad(to+1, diagPsi, diagPhi) + despPhi*despPhi
+			SPsiCuad(to+1, diagPsi, diagPhi) = SPsiCuad(to+1, diagPsi, diagPhi) + despPsi*despPsi
+			N(to+1, diagPsi, diagPhi) = N(to+1, diagPsi, diagPhi) + 1
+			
+		end do
+		diedrosant = diedros
+	end do
+	close(10)
+	print '("Analizado: [", i3, "/", i3, "]")', fileind, numfiles
+end do
+
+do waa=from, to+1
+	do dr=1, resol
+		do dc=1, resol
+			if (N(waa, dr, dc).gt.1) then
+				dSPhiPsi = dfloat(N(waa, dr, dc))*SPhiPsi(waa, dr, dc) - SPhi(waa, dr, dc)*SPsi(waa, dr, dc)
+				dSPhi = sqrt(dfloat(N(waa, dr, dc))*SPhiCuad(waa, dr, dc) - SPhi(waa, dr, dc)*SPhi(waa, dr, dc))
+				dSPsi = sqrt(dfloat(N(waa, dr, dc))*SPsiCuad(waa, dr, dc) - SPsi(waa, dr, dc)*SPsi(waa, dr, dc))
+
+				correlaciones(waa, dr, dc) = dSPhiPsi / (dSPhi*dSPsi)
+			else
+				correlaciones(waa, dr, dc) = -10
+			end if
+		end do
+	end do
+end do
+	
+! Guardado de resultados
+
+! Global
+do dr=1, resol
+	write(20,*) (correlaciones(to+1, dr, dc), dc=1, resol)		
+end do
+
+! Individual
+do waa=from, to
+	do dr=1, resol
+		write(outputs(waa),*) (correlaciones(waa, dr, dc), dc=1, resol)	
+	end do
+end do
+
+end program mapa_correlaciones
+
+
+
+
---
+++ mapasDE.f90
@@ -0,0 +1,249 @@
+program mapasDE
+
+implicit none
+
+! Datos de entrada (archivo.in)
+
+integer :: numfiles, resol, nrow, ncol
+character*200, allocatable :: filenames(:)
+
+! Definición de la matriz del diagrama
+
+double precision, allocatable :: SumaPhi(:,:), SumaPsi(:,:), SumaPhiPost(:,:), SumaPsiAnt(:,:)
+double precision, allocatable :: NPhiPsi(:,:), NPost(:,:), NAnt(:,:)
+double precision, allocatable :: MediaPhi(:,:), MediaPsi(:,:), MediaPhiPost(:,:), MediaPsiAnt(:,:)
+double precision, allocatable :: SigmaPhi(:,:), SigmaPsi(:,:), SigmaPsiAnt(:,:), SigmaPhiPost(:,:)
+double precision, allocatable :: SSigmaPhi(:,:), SSigmaPsi(:,:), SSigmaPsiAnt(:,:), SSigmaPhiPost(:,:)
+double precision :: rangoPhi, rangoPsi, dPhi, dPsi, dPhiPost, dPsiAnt
+integer :: diagPhi, diagPsi, numaa
+double precision, allocatable :: diedros(:), diedrosant(:), desps(:)
+
+! Variables de control
+
+integer ch, row, col, fileind, rowiter, aa, i, diagaa, di
+
+read(5,*) numfiles
+
+allocate(filenames(numfiles))
+
+read(5,*) filenames
+read(5,*) nrow
+read(5,*) ncol
+read(5,*) resol
+
+numaa = ncol/2
+
+allocate(diedros(ncol))
+allocate(diedrosant(ncol))
+allocate(desps(ncol))
+allocate(SumaPhi(resol, resol))
+allocate(SumaPsi(resol, resol))
+allocate(SumaPhiPost(resol, resol))
+allocate(SumaPsiAnt(resol, resol))
+allocate(NPhiPsi(resol, resol))
+allocate(NAnt(resol, resol))
+allocate(NPost(resol, resol))
+allocate(MediaPhi(resol, resol))
+allocate(MediaPsi(resol, resol))
+allocate(MediaPhiPost(resol, resol))
+allocate(MediaPsiAnt(resol, resol))
+allocate(SigmaPhi(resol, resol))
+allocate(SigmaPsi(resol, resol))
+allocate(SigmaPhiPost(resol, resol))
+allocate(SigmaPsiAnt(resol, resol))
+allocate(SSigmaPhi(resol, resol))
+allocate(SSigmaPsi(resol, resol))
+allocate(SSigmaPhiPost(resol, resol))
+allocate(SSigmaPsiAnt(resol, resol))
+
+! Matrices vacias 
+
+do row=1, resol
+	do col=1, resol
+		SumaPhi(row,col) = 0.d0
+		SumaPsi(row,col) = 0.d0
+		SumaPhiPost(row,col) = 0.d0
+		SumaPsiAnt(row,col) = 0.d0
+		SSigmaPhi(row,col) = 0.d0
+		SSigmaPsi(row,col) = 0.d0
+		SSigmaPhiPost(row,col) = 0.d0
+		SSigmaPsiAnt(row,col) = 0.d0
+		NPhiPsi(row, col) = 0.d0
+		NAnt(row, col) = 0.d0
+		NPost(row, col) = 0.d0
+	end do
+end do
+
+rangoPhi = 360/resol
+rangoPsi = 360/resol
+
+print*, 'Calculando medias'
+
+! Sumatorio de desplazamientos para calcular la media
+
+do fileind=1, numfiles
+	open(10, file = filenames(fileind))
+	read(10,*) diedrosant
+
+	do rowiter=2, nrow
+		read(10,*) diedros
+
+		do di=1, ncol
+		desps(di) = (diedros(di) - diedrosant(di))-1*int((diedros(di) - diedrosant(di))/180)*360	
+		end do
+
+		do aa=1, numaa
+			! Posición en las matrices
+			diagPhi = int((diedros(2*aa-1)+180)/rangoPhi)+1
+			diagPsi = int((diedros(2*aa)+180)/rangoPsi)+1
+			
+			if(diagPhi.ge.resol) then
+				diagPhi=resol
+			end if
+
+			if(diagPsi.ge.resol) then
+				diagPsi=resol
+			end if
+
+			dPhi = desps(2*aa -1)
+			dPsi = desps(2*aa)
+			NPhiPsi(diagPsi, diagPhi) = NPhiPsi(diagPsi, diagPhi) +1
+			SumaPhi(diagPsi, diagPhi) = SumaPhi(diagPsi, diagPhi) + dPhi
+			SumaPsi(diagPsi, diagPhi) = SumaPsi(diagPsi, diagPhi) + dPsi
+
+			if(aa.ge.2) then
+				NAnt(diagPsi, diagPhi) = NAnt(diagPsi, diagPhi) +1
+				dPsiAnt = desps(2*aa-2)
+				SumaPsiAnt(diagPsi, diagPhi) = SumaPsiAnt(diagPsi, diagPhi) + dPsiAnt
+			end if
+
+			if(aa.le.numaa-1) then
+				NPost(diagPsi, diagPhi) = NPost(diagPsi, diagPhi) +1
+				dPhiPost = desps(2*aa+1)
+				SumaPhiPost(diagPsi, diagPhi) = SumaPhiPost(diagPsi, diagPhi) + dPhiPost
+			end if
+			
+		end do
+		diedrosant = diedros
+	end do
+	close(10)
+	print '("Calculando medias: [", i3, "/", i3, "]")', fileind, numfiles
+end do
+
+! Cálculo de las medias de los desplazamientos
+
+do row=1, resol
+	do col=1, resol
+		if(NPhiPsi(row, col).gt.0) then
+			MediaPhi(row, col) = SumaPhi(row, col) / NPhiPsi(row, col)
+			MediaPsi(row, col) = SumaPsi(row, col) / NPhiPsi(row, col)
+		else
+			MediaPhi(row, col) = 0.d0
+			MediaPsi(row, col) = 0.d0
+		end if
+
+		if(NPost(row, col).gt.0) then
+			MediaPhiPost(row, col) = SumaPhiPost(row, col) / NPost(row, col)
+		else
+			MediaPhiPost(row, col) = 0.d0
+		end if
+
+		if(NAnt(row, col).gt.0) then
+			MediaPsiAnt(row, col) = SumaPsiAnt(row, col) / NAnt(row, col)
+		else
+			MediaPsiAnt(row, col) = 0.d0
+		end if
+
+	end do
+end do
+
+print*, 'Registrando desviación estándar'
+
+! Sumatorio para calcular sigma
+
+do fileind=1, numfiles
+	open(10, file = filenames(fileind))
+	read(10,*) diedrosant
+
+	do rowiter=2, nrow
+		read(10,*) diedros
+
+		do di=1, ncol
+		desps(di) = (diedros(di) - diedrosant(di))-1*int((diedros(di) - diedrosant(di))/180)*360	
+		end do
+
+		do aa=1, numaa
+			! Posición en las matrices
+			diagPhi = int((diedros(2*aa-1)+180)/rangoPhi)+1
+			diagPsi = int((diedros(2*aa)+180)/rangoPsi)+1
+			
+			if(diagPhi.ge.resol) then
+				diagPhi=resol
+			end if
+
+			if(diagPsi.ge.resol) then
+				diagPsi=resol
+			end if
+
+			dPhi = desps(2*aa -1)
+			dPsi = desps(2*aa)
+			SSigmaPhi(diagPsi,diagPhi)=SSigmaPhi(diagPsi,diagPhi)+(MediaPhi(diagPsi,diagPhi)-dPhi)**2
+			SSigmaPsi(diagPsi,diagPhi)=SSigmaPsi(diagPsi,diagPhi)+(MediaPsi(diagPsi,diagPhi)-dPsi)**2
+
+			if(aa.ge.2) then
+				dPsiAnt = desps(2*aa-2)
+				SSigmaPsiAnt(diagPsi,diagPhi)=SSigmaPsiAnt(diagPsi,diagPhi)+(MediaPsiAnt(diagPsi,diagPhi)-dPsiAnt)**2
+			end if
+
+			if(aa.le.numaa-1) then
+				dPhiPost = desps(2*aa+1)
+				SSigmaPhiPost(diagPsi,diagPhi)=SSigmaPhiPost(diagPsi,diagPhi)+(MediaPhiPost(diagPsi,diagPhi)-dPhiPost)**2
+			end if
+			
+		end do
+		diedrosant = diedros
+	end do
+	close(10)
+	print '("Registrando desviación estándar: [", i3, "/", i3, "]")', fileind, numfiles
+end do
+
+! Calculando las desviaciones estándar
+
+do row=1, resol
+	do col=1, resol
+		if(NPhiPsi(row, col).gt.0) then
+			SigmaPhi(row, col) = sqrt(SSigmaPhi(row, col)/NPhiPsi(row, col))
+			SigmaPsi(row, col) = sqrt(SSigmaPsi(row, col)/NPhiPsi(row, col))
+		else
+			SigmaPhi(row, col) = -10
+			SigmaPsi(row, col) = -10
+		end if
+
+		if(NPost(row, col).gt.0) then
+			SigmaPhiPost(row, col) = sqrt(SSigmaPhiPost(row, col)/NPost(row, col))
+		else
+			SigmaPhiPost(row, col) = -10
+		end if
+
+		if(NAnt(row, col).gt.0) then
+			SigmaPsiAnt(row, col) = sqrt(SSigmaPsiAnt(row, col)/NAnt(row, col))
+		else
+			SigmaPsiAnt(row, col) = -10
+		end if
+	end do
+end do
+
+! Escribiendo los resultados en sus archivos
+
+do row=1, resol
+	write(20,*) (SigmaPhi(row, col), col=1, resol)
+	write(21,*) (SigmaPsi(row, col), col=1, resol)
+	write(22,*) (SigmaPhiPost(row, col), col=1, resol)
+	write(23,*) (SigmaPsiAnt(row, col), col=1, resol)
+end do
+
+end program mapasDE
+
+
+
+
